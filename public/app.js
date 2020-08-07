fe429ffc9d004cd7709afc692a0a60a0025df138
Marco Frison <riazaford@gmail.com>
2020-08-07 07:36:55 +0200

    Implementazione autenticazione

--- public/app.js
+++ public/app.js
@@ -36,6 +36,74 @@ db.collection("registrazioni")
 */
 const db = firebase.firestore();
 
+  
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // User successfully signed in.
+        // Return type determines whether we continue the redirect automatically
+        // or whether we leave that to developer to handle.
+        return true;
+      },
+      uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      //firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    //tosUrl: '<your-tos-url>',
+    // Privacy policy url.
+    //privacyPolicyUrl: '<your-privacy-policy-url>'
+  };
+
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
+window.onunload = function () {
+    firebase.auth()
+    .signOut()
+    .then(function() {
+        // Sign-out successful.
+      })
+    .catch(function(error) {
+        
+        // An error happened.
+      });
+      
+};
+function logOut(){
+    firebase.auth()
+    .signOut()
+    .catch(function(error) {
+        console.log(error);
+        // An error happened.
+      });
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      document.getElementById("userName").innerHTML = user.displayName +  "</br>Log out";
+      // ...
+    } else {
+      // User is signed out.
+      console.log("User logged out");
+      document.getElementById("userName").innerHTML = "Log in";
+    }
+  });
+
 const regs = [
     document.getElementById("timeEntMor"),
     document.getElementById("timeExitMor"),
